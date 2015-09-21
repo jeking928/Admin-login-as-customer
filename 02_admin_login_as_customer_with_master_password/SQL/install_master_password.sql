953e864a125f1a080e6c73b8223f14fd78810182
James King <jeking@wheatonwebsiteservices.com>
2015-09-20 21:57:02 -0500

    Initial Commit

---
+++ 02_admin_login_as_customer_with_master_password/SQL/install_master_password.sql
@@ -0,0 +1,1 @@
+INSERT INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES ('Master Password', 'MASTER_PASS', 'yourpassword', 'This password will allow you to login to any customer\'s account.', 1, 23, now(), now(), NULL, NULL);
